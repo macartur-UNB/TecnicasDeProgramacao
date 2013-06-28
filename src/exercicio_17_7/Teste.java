52b7178651dbf720f0af146f0c44a0200226bd6d
Alunos <alunos@gama-A410-G-BE45P1.(none)>
2013-06-28 14:53:09 -0300

    Exercicio 17.7

---
+++ src/exercicio_17_7/Teste.java
@@ -0,0 +1,20 @@
+package exercicio_17_7;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Programa p1 = new Programa();
+		p1.setId(1);
+		
+		Thread t1 = new Thread(p1);
+		t1.start();
+		
+		Programa p2 = new Programa();
+		p2.setId(2);
+		
+		Thread t2 = new Thread(p2);
+		t2.start();
+	}
+
+}
