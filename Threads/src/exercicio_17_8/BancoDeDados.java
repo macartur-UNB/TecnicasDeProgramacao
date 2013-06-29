b0d0734e5d9a4c9544c85208a55a0c2ecc839687
macartur <macartur.sc@gmail.com>
2013-06-29 15:00:24 -0300

    PROGRAMAÇÂO PARALELA COM THREADS

---
+++ Threads/src/exercicio_17_8/BancoDeDados.java
@@ -0,0 +1,44 @@
+package exercicio_17_8;
+
+import java.util.ArrayList;
+import java.util.Collection;
+//import java.util.HashSet;
+//import java.util.Vector;
+
+public class BancoDeDados {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		Collection<String> sqls = new ArrayList<String>();
+		
+		ProduzSQL p1 = new ProduzSQL(0, 10000, sqls);
+		Thread t1 = new Thread(p1);
+		
+		ProduzSQL p2 = new ProduzSQL(10000, 20000,sqls);
+		Thread t2 = new Thread(p2);
+		
+		ProduzSQL p3 = new ProduzSQL(20000, 30000, sqls);
+		Thread t3 = new Thread(p3);
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		
+		t1.join();
+		t2.join();
+		t3.join();
+		
+		System.out.println("Threads produtoras de sqls finalizadas");
+		
+		for(int i = 0 ; i< 30000; i++){
+			if(!sqls.contains("SQL"+i)){
+				throw new IllegalStateException("Não encontrei SQL");
+			}
+		}		
+			if(sqls.contains(null)){
+				throw new IllegalStateException("Não deveria ter null aqui dentro");
+			}
+			System.out.println("Fim da execução com sucesso");
+	}
+
+}
---
+++ Threads/src/exercicio_17_8/ProduzSQL.java
@@ -0,0 +1,24 @@
+package exercicio_17_8;
+
+import java.util.Collection;
+
+public class ProduzSQL implements Runnable{
+	private int comeco;
+	private int fim;
+	private Collection<String> sqls;
+	
+		public ProduzSQL(int comeco, int fim, Collection<String> sqls2) {
+			this.comeco = comeco;
+			this.fim = fim;
+			this.sqls = sqls2;
+		}
+		
+		public void run() {
+			for (int i = comeco; i < fim; i++) {
+				synchronized(sqls){
+					sqls.add("SQL"+i);
+				}
+		}
+	}
+}
+
